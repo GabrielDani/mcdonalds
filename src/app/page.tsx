f4eb6d64dd912a9eba0579cfe552ec2f47403570
Gabriel Dani <gabriel.dani07@gmail.com>
2025-06-23 19:32:15 +0000

    chore: restautant-card component and restaurant length shown

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,6 @@
 import { Restaurant } from "@prisma/client";
-import Image from "next/image";
 
+import { RestaurantCard } from "@/components/ui/restaurant-card";
 import { db } from "@/lib/prisma";
 
 const HomePage = async () => {
@@ -8,41 +8,23 @@ const HomePage = async () => {
 
   return (
     <main className="mx-auto max-w-5xl space-y-6 p-6">
-      <h1 className="text-3xl font-bold text-yellow-600">Unidades McDonalds</h1>
+      <section className="mb-6 flex flex-col items-center justify-center gap-2 text-center sm:flex-row sm:gap-4 sm:text-left">
+        <h1 className="text-3xl font-bold text-yellow-600">
+          Unidades McDonalds
+        </h1>
+        <p className="text-sm text-gray-700">
+          (<span className="font-semibold">{restaurants.length}</span>{" "}
+          {restaurants.length > 1 ? "restaurantes" : "restaurante"})
+        </p>
+      </section>
 
       {restaurants.length === 0 && (
         <p className="text-gray-500">Nenhum restaurante encontrado.</p>
       )}
 
       <ul className="grid grid-cols-1 gap-6 md:grid-cols-2">
-        {restaurants.map((restaurant) => (
-          <li
-            key={restaurant.id}
-            className="flex flex-col gap-4 rounded-2xl bg-white p-4 shadow"
-          >
-            <Image
-              src={restaurant.coverImageUrl}
-              alt={restaurant.name}
-              width={600}
-              height={200}
-              className="h-40 w-full rounded-xl object-cover"
-            />
-            <div>
-              <h2 className="text-xl font-semibold">{restaurant.name}</h2>
-              <p className="text-sm text-gray-600">
-                {restaurant.description || "Restaurante da rede McDonald's"}
-              </p>
-              <p className="mt-2 text-sm text-gray-500">
-                Aberto das 10h às 23h
-              </p>
-            </div>
-            <a
-              href={`/${restaurant.slug}`}
-              className="mt-auto inline-block rounded-lg bg-yellow-500 py-2 text-center font-medium text-white transition hover:bg-yellow-600"
-            >
-              Ver cardápio
-            </a>
-          </li>
+        {restaurants.map((restaurant, index) => (
+          <RestaurantCard key={index} restaurant={restaurant} />
         ))}
       </ul>
     </main>
---
+++ src/components/ui/restaurant-card.tsx
@@ -0,0 +1,36 @@
+import { Restaurant } from "@prisma/client";
+import Image from "next/image";
+
+interface RestaurantCardProps {
+  restaurant: Restaurant;
+}
+
+export const RestaurantCard = ({ restaurant }: RestaurantCardProps) => {
+  return (
+    <li
+      key={restaurant.id}
+      className="flex flex-col gap-4 rounded-2xl bg-white p-4 shadow"
+    >
+      <Image
+        src={restaurant.coverImageUrl}
+        alt={restaurant.name}
+        width={600}
+        height={200}
+        className="h-40 w-full rounded-xl object-cover"
+      />
+      <div>
+        <h2 className="text-xl font-semibold">{restaurant.name}</h2>
+        <p className="text-sm text-gray-600">
+          {restaurant.description || "Restaurante da rede McDonald's"}
+        </p>
+        <p className="mt-2 text-sm text-gray-500">Aberto das 10h às 23h</p>
+      </div>
+      <a
+        href={`/${restaurant.slug}`}
+        className="mt-auto inline-block rounded-lg bg-yellow-500 py-2 text-center font-medium text-white transition hover:bg-yellow-600"
+      >
+        Ver cardápio
+      </a>
+    </li>
+  );
+};
